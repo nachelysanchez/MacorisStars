b0a6d36ffc51c49ca88e29a8ce2156dc4419381e
Nachely Sánchez <nashely-5@hotmail.com>
2024-07-09 22:10:46 -0400

    Creando contexto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using MacoriStars.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MacoriStars.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Resenas> Resenas { get; set; }
+        public DbSet<Calificaciones> Calificaciones { get; set; }
+        public DbSet<Establecimientos> Establecimientos { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
