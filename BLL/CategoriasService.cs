eaccdaf6944e30bb8d1a277dbb4d7d54467cb60f
Nachely Sánchez <nashely-5@hotmail.com>
2024-07-09 22:21:54 -0400

    Creando interfaz del Servicio de Categorias

---
+++ BLL/CategoriasService.cs
@@ -0,0 +1,37 @@
+﻿using MacoriStars.DAL;
+using MacoriStars.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MacoriStars.BLL
+{
+    public class CategoriasService : ICategoriasServices
+    {
+        private readonly Contexto contexto;
+
+        public CategoriasService(Contexto _contexto)
+        {
+            this.contexto = _contexto;
+        }
+        /// <summary>
+        /// Permite buscar una tarea mediante id
+        /// </summary>
+        /// <param name="id">El Id de la entidad que se desea buscar</param>
+        public async Task<Categorias> GetCategoria(int idCategoria)
+        {
+            var categoria = await contexto.Categorias
+                .Where(x=> x.IdCategoria == idCategoria)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            return categoria;
+        }
+        /// <summary>
+        /// Permite buscar una tarea mediante id
+        /// </summary>
+        /// <param name="id">El Id de la entidad que se desea buscar</param>
+        public async Task<List<Categorias>> GetCategorias()
+        {
+            var categorias = await contexto.Categorias.ToListAsync();
+            return categorias;
+        }
+    }
+}
---
+++ BLL/ICategoriasServices.cs
@@ -0,0 +1,10 @@
+﻿using MacoriStars.Models;
+
+namespace MacoriStars.BLL
+{
+    public interface ICategoriasServices
+    {
+        Task<List<Categorias>> GetCategorias();
+        Task<Categorias> GetCategoria(int idCategoria);
+    }
+}
