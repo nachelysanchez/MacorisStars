2a7a0c95fe501f93b7b889e1fe50c6891fa6fae7
Nachely Sánchez <nashely-5@hotmail.com>
2024-07-09 22:34:01 -0400

    Agregando servicio de establecimientos y interfaz

---
+++ BLL/EstablecimientosService.cs
@@ -0,0 +1,47 @@
+﻿using MacoriStars.DAL;
+using MacoriStars.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MacoriStars.BLL
+{
+    public class EstablecimientosService : IEstablecimientosService
+    {
+        private readonly Contexto contexto;
+
+        public EstablecimientosService(Contexto _contexto)
+        {
+            this.contexto = _contexto;
+        }
+        public async Task<Establecimientos> GetEstablecimiento(int idEstablecimiento)
+        {
+            try
+            {
+                var establecimiento = await contexto.Establecimientos
+                    .Where(x => x.IdEstablecimiento == idEstablecimiento)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync();
+                return establecimiento;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<Establecimientos>> GetEstablecimientos()
+        {
+            var establecimientos = await contexto
+                .Establecimientos
+                .ToListAsync();
+            return establecimientos;
+        }
+
+        public async Task<List<Establecimientos>> GetList(Expression<Func<Establecimientos, bool>> criterio)
+        {
+            var lista = await contexto.Establecimientos.Where(criterio).ToListAsync();
+            return lista;
+        }
+    }
+}
---
+++ BLL/IEstablecimientosService.cs
@@ -0,0 +1,12 @@
+﻿using MacoriStars.Models;
+using System.Linq.Expressions;
+
+namespace MacoriStars.BLL
+{
+    public interface IEstablecimientosService
+    {
+        Task<Establecimientos> GetEstablecimiento(int idEstablecimiento);
+        Task<List<Establecimientos>> GetEstablecimientos();
+        Task<List<Establecimientos>> GetList(Expression<Func<Establecimientos, bool>> criterio);
+    }
+}
