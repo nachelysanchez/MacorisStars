20f8af0d58e0408793f09acbed7f2a7c52573cb3
Nachely Sánchez <nashely-5@hotmail.com>
2024-07-09 22:45:59 -0400

    Creando DTO de Sesion

---
+++ Dtos/SesionDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MacoriStars.Dtos
+{
+    public class SesionDto
+    {
+        public static int IdUsuarioLogueado { get; set; }
+        public void SetUsuarioLog(int idUsuario)
+        {
+            IdUsuarioLogueado = idUsuario;
+        }
+
+        public int GetUsuarioLog()
+        {
+            return IdUsuarioLogueado;
+        }
+    }
+}
