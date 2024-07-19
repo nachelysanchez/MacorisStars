09c1ea39daab62dfac31de90c79f345b283c643b
Nachely Sánchez <nashely-5@hotmail.com>
2024-07-19 05:49:42 -0400

    Agregando vista de restaurantes

---
+++ Dtos/CardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MacoriStars.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+    }
+}
